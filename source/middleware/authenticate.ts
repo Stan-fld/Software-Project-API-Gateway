ca397cb9f57032c33c67f3df31e1c1685f7bfc0a
Stan-fld <stanislas.foillard@viacesi.fr>
2022-06-22 15:38:45 +0200

    fixed get client ip set by nginx

--- source/middleware/authenticate.ts
+++ source/middleware/authenticate.ts
@@ -4,7 +4,7 @@ import {createError} from "../server/errors/errors";
 
 export function authenticateIp(req: any, res: any, next: any) {
 
-    const clientIp = req.header('X-Real-IP')
+    const clientIp = req.header('x-real-ip')
 
     if (isBlackList(clientIp)) {
         return res.status(403).send(createError('IpBlackListed', 'The supplied ip is blackListed', 403));
